9b4d208b6811d7b8b7c8a39ff6282f4dcb9dc773
Cyabanz <118572258+Cyabanz@users.noreply.github.com>
2024-09-29 10:23:51 -0500

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,16 +1,8 @@
 // Theme switcher
-
 document.addEventListener('DOMContentLoaded', function() {
   function theme(name) {
       let root = document.querySelector(':root');
-      let particles = document.getElementById('particles-js');
-      if (name === 'default') {
-        root.className = '';
-        particles.className = '';
-      } else {
-        root.className = name;
-        particles.className = name;
-      }
+      root.className = name === 'default' ? '' : name;
       localStorage.setItem('theme', name);
   }  
   
@@ -29,21 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const savedOption = localStorage.getItem('option');
   if (savedOption) {
-      select.value = savedOption;
-      const selectedOption = select.options[select.selectedIndex];
-      const themeName = selectedOption.getAttribute('data-theme');
-      theme(themeName);
-}
+    select.value = savedOption;
+    const selectedOption = select.options[select.selectedIndex];
+    const themeName = selectedOption.getAttribute('data-theme');
+    theme(themeName);
+  }
 });
 
-// tab title and icon cloak function
-
+// Tab title and icon cloak function
 function cloak(icon, title) {
   var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
   link.type = 'image/x-icon';
   link.rel = 'shortcut icon';
   link.href = icon;
-  document.title= title;
+  document.title = title;
   document.getElementsByTagName('head')[0].appendChild(link);
   
   localStorage.setItem('icon', icon);
@@ -57,73 +48,66 @@ if (savedIcon && savedTitle) {
 } 
 
 // Anti Close function
-
 let savedAntiClose = localStorage.getItem('antiClose');
 if (savedAntiClose === 'true') {
-document.getElementById('anti-close-checkbox').checked = true;
-antiClose();
+  document.getElementById('anti-close-checkbox').checked = true;
+  antiClose();
 }
 
 document.getElementById('anti-close-checkbox').addEventListener('change', function() {
-localStorage.setItem('antiClose', 'true');
-
-if (localStorage.getItem('antiClose')) {
-  antiClose();
-} else {
-  localStorage.removeItem('antiClose');
-}
+  if (this.checked) {
+    localStorage.setItem('antiClose', 'true');
+    antiClose();
+  } else {
+    localStorage.removeItem('antiClose');
+  }
 });
 
 function antiClose() {
   if (document.getElementById('anti-close-checkbox').checked) {
-      window.top.addEventListener('beforeunload',function(e){
-          e.preventDefault();
-          e.returnValue='Are you sure you want to leave?';
-      });
-  } else {
-      localStorage.removeItem('antiClose');
+    window.top.addEventListener('beforeunload', function(e) {
+      e.preventDefault();
+      e.returnValue = 'Are you sure you want to leave?';
+    });
   }
 }
 
 // About Blank function
-
 let firstAboutBlank = localStorage.getItem('aboutBlank');
 if (firstAboutBlank === 'true') {
-document.getElementById('about-blank').checked = true;
-aboutBlank();
+  document.getElementById('about-blank').checked = true;
+  aboutBlank();
 }
 
 document.getElementById('about-blank').addEventListener('change', function() {
-if (localStorage.getItem('aboutBlank')) {
-
-
-  localStorage.removeItem('aboutBlank');
-} else {
-  localStorage.setItem('aboutBlank', 'true');
-  aboutBlank();
-}
+  if (this.checked) {
+    localStorage.setItem('aboutBlank', 'true');
+    aboutBlank();
+  } else {
+    localStorage.removeItem('aboutBlank');
+  }
 });
 
 function aboutBlank() {
-if (document.getElementById('about-blank').checked) {
-  var windowUrl = window.location.href;
-  var newWindow = window.open("about:blank", "_blank");
-  if (newWindow != null) { // add null check here
-    var newContent = document.createElement("iframe");
-    newContent.src = windowUrl;
-    newContent.style.width = "100vw";
-    newContent.style.height = "100vh";
-    newContent.style.border = "none";
-    newContent.style.position = "absolute";
-    newContent.style.top = "0";
-    newContent.style.left = "0";
-    newWindow.document.body.appendChild(newContent);
-    setTimeout(function() {
-      window.location.replace("https://classroom.google.com/u/0/h");
-    }, 100);
+  if (document.getElementById('about-blank').checked) {
+    var windowUrl = window.location.href;
+    var newWindow = window.open("about:blank", "_blank");
+    if (newWindow != null) {
+      var newContent = document.createElement("iframe");
+      newContent.src = windowUrl;
+      newContent.style.width = "100vw";
+      newContent.style.height = "100vh";
+      newContent.style.border = "none";
+      newContent.style.position = "absolute";
+      newContent.style.top = "0";
+      newContent.style.left = "0";
+      newWindow.document.body.appendChild(newContent);
+      setTimeout(function() {
+        window.location.replace("https://classroom.google.com/u/0/h");
+      }, 100);
+    }
+  } else {
+    localStorage.removeItem('aboutBlank');
+    window.close();
   }
-} else {
-  localStorage.removeItem('aboutBlank');
-  window.close()
-}
 }
