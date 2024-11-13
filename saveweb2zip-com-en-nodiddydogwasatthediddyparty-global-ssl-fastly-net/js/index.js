d1670e1d7bcb6bfac118be89e13fcf2f4758f346
Cyabanz <118572258+Cyabanz@users.noreply.github.com>
2024-11-12 21:01:14 -0600

    Add files via upload

---
+++ saveweb2zip-com-en-nodiddydogwasatthediddyparty-global-ssl-fastly-net/js/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if(window.location.protocol === 'http:'){
+        if (window.location.host === 'localhost'){
+    
+        }
+        else if (window.location.host === '127.0.0.1'){
+    
+        }
+        else{
+            document.getElementById('uv-address').disabled = true;
+            document.getElementById('uv-address').placeholder = 'proxy disabled! we are NOT on https.';
+        }
+    }
+});
+
+const randomtextstuff = [
+    "rizz?",
+    "DOWN WITH BLOCKERS",
+    '"faggot!" - szvy',
+    "also try getaway shootout",
+    "official frogie's arcade song goes hard",
+    "we love frogie's arcade",
+    "save your data!",
+    "we dont talk about TBG95.",
+    '"may you please add roblox???🥺"',
+    "join the discord!",
+    "search freely....",
+    "all unblocked!",
+    "oil up lil bro",
+    "fa v4 when?",
+    "stephanie.pillers@cfisd.net",
+    "calvillo5432ud@gmail.com",
+    "this is indeed a proxy",
+    "whats on your mind?"
+];
